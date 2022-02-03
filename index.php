e2e6d5620a25e4d39264c834c766dd09cba3556e
ABDULGANIASYUKUR <93692928+ABDULGANIASYUKUR@users.noreply.github.com>
2022-02-03 09:30:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include("konten/head.html");
+    ?>
+</head>
+<body>
+
+    <div class="container">
+        <?php
+        include("konten/judul.html");
+        include("konten/nav.html");
+
+        include("atur-konten.php");|
+        ?>
+        </div>
+        
+</body>
+</html>
